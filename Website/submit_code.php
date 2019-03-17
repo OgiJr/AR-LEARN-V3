5f7e373fe9f92695be972e8705073a25e3544477
BobbyRaduloff <bobbyraduloff@gmail.com>
2019-03-17 12:51:48 +0200

    Debugging submission.

--- Website/submit_code.php
+++ Website/submit_code.php
@@ -1,6 +1,10 @@
 <?php
 	require_once 'PostNewTarget.php';
 
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
 	// Set utf8 support
 	header('Content-Type: text/html; charset=utf-8');
 	ini_set("default_charset", "UTF-8");
