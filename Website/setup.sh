1b1677970e9a6af1b9d41e9d95395f82a6c588a8
BobbyRaduloff <bobbyraduloff@gmail.com>
2019-03-17 13:00:35 +0200

    Made it so setup.sh doesn't interfere with git pull on server.

--- Website/setup.sh
+++ Website/setup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-cd ..
-git stash
-cd Website
-
 if [ "$#" -ne 3 ]; then
         echo "USAGE: ./setup.sh [password] [vuforia access keys] [vuforia secret key]"
         exit 1
@@ -10,3 +6,7 @@ fi
 sed -i "s/PASSWORD/${1}/g" submit_code.php
 sed -i "s/ACCESSKEY/${2}/g" submit_code.php
 sed -i "s/SERVERKEY/${3}/g" submit_code.php
+
+cd ..
+git stash
+cd Website
