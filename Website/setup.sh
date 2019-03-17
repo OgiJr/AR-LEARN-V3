c98b9dd5c66cab418623202b5a0ae5d223083b34
BobbyRaduloff <bobbyraduloff@gmail.com>
2019-03-17 12:48:01 +0200

    Fixed the last bug in this setup script. Apparently, ${0} is the name of the script.

--- Website/setup.sh
+++ Website/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if ["$#" -ne 3]; then
-	echo "USAGE: ./setup.sh [password] [vuforia access keys] [vuforia secret key]"
-	exit 1
+if [ "$#" -ne 3 ]; then
+        echo "USAGE: ./setup.sh [password] [vuforia access keys] [vuforia secret key]"
+        exit 1
 fi
-sed -i "s/<PASSWORD>/${0}/g" submit_code.php
-sed -i "s/<ACCESSKEY>/${1}/g" submit_code.php
-sed -i "s/<SERVERKEY>/${2}/g" submit_code.php
+sed -i "s/PASSWORD/${1}/g" submit_code.php
+sed -i "s/ACCESSKEY/${2}/g" submit_code.php
+sed -i "s/SERVERKEY/${3}/g" submit_code.php
