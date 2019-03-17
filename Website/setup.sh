ee14c6426908a52b0fa1a186e5d6278b409a29c8
BobbyRaduloff <bobbyraduloff@gmail.com>
2019-03-17 12:24:27 +0200

    Fixed bug in setup.sh

--- Website/setup.sh
+++ Website/setup.sh
@@ -4,6 +4,6 @@ then
 	echo "USAGE: ./setup.sh [password] [vuforia access keys] [vuforia secret key]"
 	exit 1
 fi
-sed -i "s/<PASSWORD>/$0/g" submit_code.php
-sed -i "s/<ACCESSKEY>/$1/g" submit_code.php
-sed -i "s/<SERVERKEY>/$2/g" submit_code.php
+sed -i "s/<PASSWORD>/${0}/g" submit_code.php
+sed -i "s/<ACCESSKEY>/${1}/g" submit_code.php
+sed -i "s/<SERVERKEY>/${2}/g" submit_code.php
