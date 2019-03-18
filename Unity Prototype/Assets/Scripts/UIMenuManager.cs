b6cb75755224db87256a90999aacc72d02486671
Pucanica Developer <thefriendlyguy123@gmail.com>
2019-03-19 01:50:31 +0200

    UI System Menu
    This new version creates a new UI System where you can choose which game object you want in offline mode and displays more information about the object.

---
+++ Unity Prototype/Assets/Scripts/UIMenuManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIMenuManager : MonoBehaviour
+{
+    private int state = 0;
+    public GameObject[] main = new GameObject[3];
+
+    public void MinusOne()
+    {
+        if(state == 0)
+        {
+            main[0].GetComponent<RectTransform>().DOAnchorPos(new Vector3(1200, 0, 0), .5f);
+            main[1].GetComponent<RectTransform>().DOAnchorPos(new Vector3(450, -700, 0), .5f);
+        }
+        if (state == 1)
+        {
+            main[0].GetComponent<RectTransform>().DOAnchorPos(new Vector3(0, 0, 0), .5f);
+            main[2].GetComponent<RectTransform>().DOAnchorPos(new Vector3(2000, -700, 0), .5f);
+        }
+        state--;
+    }
+
+    public void PlusOne()
+    {
+        if(state == -1)
+        {
+            main[0].GetComponent<RectTransform>().DOAnchorPos(Vector3.zero, .5f);
+            main[1].GetComponent<RectTransform>().DOAnchorPos(new Vector3(-450, -700, 0), .5f);
+        }
+
+        if (state == 0)
+        {
+            main[0].GetComponent<RectTransform>().DOAnchorPos(new Vector3(-1200, 0, 0), .5f);
+            main[2].GetComponent<RectTransform>().DOAnchorPos(new Vector3(450, -700, 0), .5f);
+        }
+
+        state++;
+    }
+}
