a6324e495ebfbb8ed6e77760ac8bc49c15912c32
BobbyRaduloff <bobbyraduloff@gmail.com>
2019-03-27 23:07:26 +0200

    Fix a bug or two.

--- Website/AssetBuilder/Assets/AssetBuilder.cs
+++ Website/AssetBuilder/Assets/AssetBuilder.cs
@@ -10,24 +10,25 @@ public class AssetBuilder : Editor
         String[] arguments = Environment.GetCommandLineArgs();
         string id = arguments[7];
         int num = Int32.Parse(arguments[8]);
-        Object[] objs = new Object[num];
-        Object[] texts = new Object[num];
-        Object[] final = new Object[num * 2];
+        UnityEngine.Object[] objs = new UnityEngine.Object[num];
+        UnityEngine.Object[] texts = new UnityEngine.Object[num];
+        UnityEngine.Object[] final = new UnityEngine.Object[num * 2];
 
         int j = 0;
         for(int i = 0; i < num; i++)
         {
-            objs[i] = Resources.Load<Object>("markdown/" + id + "_" + i);
-            texts[i] = Resources.Load<Object>("models/" + id + "_" + i);
+            objs[i] = Resources.Load<UnityEngine.Object>("markdown/" + id + "_" + i);
+            texts[i] = Resources.Load<UnityEngine.Object>("models/" + id + "_" + i);
             final[j++] = objs[i];
             final[j++] = texts[i];
         }
 
         string bundlePath = "Assets/AssetBundle/" + id + ".unity3d";
-        BuildPipeline.BuildAssetBundles(
+        BuildPipeline.BuildAssetBundle(
             final[0],
             final,
             bundlePath,
+            BuildAssetBundleOptions.None,
             BuildTarget.Android);
     }
 }
