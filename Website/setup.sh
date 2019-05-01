8dbb0b7037b06e0f8c7bb1061bc951f19bb4d112
BobbyRaduloff <bobbyraduloff@gmail.com>
2019-05-01 18:53:01 +0300

    Fixed a bug in the deplyoment script

--- Website/setup.sh
+++ Website/setup.sh
@@ -6,5 +6,6 @@ fi
 sed -i "s/PASSWORD/${1}/g" submit.php
 sed -i "s/PASSWORD/${1}/g" submit_vocab.php
 sed -i "s/PASSWORD/${1}/g" getinfo.php
+sed -i "s/PASSWORD/${1}/g" dictionary.php
 sed -i "s/ACCESSKEY/${2}/g" submit.php
 sed -i "s/SERVERKEY/${3}/g" submit.php
