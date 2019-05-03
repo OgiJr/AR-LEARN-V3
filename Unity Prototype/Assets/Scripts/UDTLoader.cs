88d6d5c09133112a0087608f1dca421b728b9c8d
Pucanica Developer <thefriendlyguy123@gmail.com>
2019-05-03 23:59:40 +0300

    Fix 2

---
+++ Unity Prototype/Assets/Scripts/UDTLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Loads downloader AR models at arlearn.xyz
+/// </summary>
+public class UDTLoader : MonoBehaviour
+{
+    public GameObject current;
+    private GameObject errorUI;
+
+    public GameObject load(string name)
+    {
+        errorUI = GameObject.Find("ErrorUI");
+        if(!System.IO.File.Exists(Application.persistentDataPath + "/assets/" + name + ".unity3d")){
+            errorUI.SetActive(true);
+        }
+        AssetBundle ab = AssetBundle.LoadFromFile(Application.persistentDataPath + "/assets/" + name + ".unity3d");
+        current = Instantiate(ab.mainAsset as GameObject);
+        return current;
+    }
+}
